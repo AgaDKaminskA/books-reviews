e5e01ec46920143b2c50d12fe546418831b722fc
agadkaminska <120246524+AgaDKaminskA@users.noreply.github.com>
2023-04-02 10:33:23 +0000

    nowe_testy

--- test.py
+++ test.py
@@ -20,6 +20,14 @@ def test_review_add():
         })
         assert response.status_code == 200
 
+def test_review_not_add():
+    with app.test_client() as client:
+        response = client.post('/add-review', json={
+            'book_name': 'ASDF',
+            'review': 'QWERTY'
+        })
+        assert response.status_code == 500
+
 def test_get_book_id():
     with app.test_client() as client:
         response = client.post('/search', json={
@@ -28,3 +36,25 @@ def test_get_book_id():
         assert response.status_code == 200
         assert int(response.json['book_id']) == 1
 
+#def test_review_add_to_long():
+    #with app.test_client() as client:
+        #response = client.post('/add-review', json={
+            #'book_name': 'ASDF',
+            #'rating': 3,
+            #'review': 'QWERTY' * 500
+            #}
+
+
+def test_multiple_reviews():
+    with app.test_client() as client:
+        for i in range (3):
+            response = client.post('/add-review', json={
+            'book_name': 'ASDF',
+            'rating': 3,
+            'review': 'QWERTY'
+            })
+
+    response = client.get('/most-popular')
+    json = response.json
+    json_obj = json[0]
+    assert json_obj ['review_count'] == 3
